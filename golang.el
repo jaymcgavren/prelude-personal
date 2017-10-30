8b78564e9a3cd8bb3030b29b8b9ff1575a9ecadd
Jay McGavren <jay@mcgavren.com>
2017-10-30 16:47:59 -0700

    Add golint.

--- golang.el
+++ golang.el
@@ -4,6 +4,8 @@
   (add-hook 'before-save-hook 'gofmt-before-save)
   (setq tab-width 4 indent-tabs-mode 1))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
+(add-to-list 'load-path (concat (getenv "HOME")  "/go/src/github.com/golang/lint/misc/emacs"))
+(require 'golint)
 
 ;; Tabs are not a whitespace error in .go files; don't treat them as such.
 (setq whitespace-global-modes '(not go-mode))
