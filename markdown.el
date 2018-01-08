3a66a7678a54cdd9d465c7837b79d2b6f1ef4eda
Jay McGavren <jay@mcgavren.com>
2018-01-07 17:08:13 -0700

    Highlight level 1 and 2 headings in markdown-mode.

--- markdown.el
+++ markdown.el
@@ -3,3 +3,9 @@
 (add-hook 'markdown-mode-hook 'my-markdown-mode-hook)
 
 (setq whitespace-global-modes '(not markdown-mode))
+
+(custom-set-faces
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :background "brightwhite" :foreground "blue"))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :background "brightwhite" :foreground "blue"))))
+)
