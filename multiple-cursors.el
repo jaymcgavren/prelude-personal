cfad2677a378c60aae2114e417a73729cb5bc17e
Jay McGavren <jay@mcgavren.com>
2021-10-06 16:39:46 -0700

    Uninstall multiple-cursors

--- multiple-cursors.el
+++
@@ -1,6 +0,0 @@
-(require 'multiple-cursors)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-M->") 'mc/unmark-next-like-this)
-(global-set-key (kbd "C-M-<") 'mc/unmark-previous-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
