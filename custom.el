70238f5b131814023f3af875112ff962b1368b43
Jay McGavren <jay@mcgavren.com>
2021-10-06 16:18:55 -0700

    Change highlight colors for Markdown headers

--- custom.el
+++ custom.el
@@ -10,4 +10,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold))))
+ '(markdown-header-face-1 ((t (:inherit markdown-header-face :background "brightwhite" :foreground "blue"))))
+ '(markdown-header-face-2 ((t (:inherit markdown-header-face :background "brightwhite" :foreground "blue")))))
