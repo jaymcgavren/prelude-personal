af67eb082a6778c1a07012f223e4512be161c66d
Jay McGavren <jay@mcgavren.com>
2021-10-06 16:23:16 -0700

    Extend midnight-mode grace period to 4 days

--- init.el
+++ init.el
@@ -50,7 +50,7 @@
 ;; Kill special buffers untouched for X SECONDS.
 (setq clean-buffer-list-delay-special 300)
 ;; Kill file buffers untouched for X DAYS.
-(setq clean-buffer-list-delay-general 3)
+(setq clean-buffer-list-delay-general 4)
 ;; Run clean-buffer-list every hour.
 (setq clean-buffer-list-timer (run-at-time t 3600 'clean-buffer-list))
 
