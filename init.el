3e77bedf108fda6ecf6d685cee51968ee3f08be3
Jay McGavren <jay@mcgavren.com>
2024-04-28 14:31:21 -0400

    Extract midnight-mode config from init.el

--- init.el
+++ init.el
@@ -19,14 +19,5 @@
 (global-set-key (kbd "C-c t") 'tgt-toggle)
 (setq tgt-open-in-new-window 'nil)
 
-;; midnight-mode kills unused buffers.
-(require 'midnight)
-;; Kill special buffers untouched for X SECONDS.
-(setq clean-buffer-list-delay-special 300)
-;; Kill file buffers untouched for X DAYS.
-(setq clean-buffer-list-delay-general 4)
-;; Run clean-buffer-list every hour.
-(setq clean-buffer-list-timer (run-at-time t 3600 'clean-buffer-list))
-
 ;; Keep magit from asking which remote branch to push to.
 (setq magit-push-always-verify nil)
