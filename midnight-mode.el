b29ba1808e25808c28ee60fb10f99c9fec7c117d
Jay McGavren <jay@mcgavren.com>
2024-04-28 14:31:54 -0400

    Disable midnight-mode (automatic buffer closures)

--- midnight-mode.el
+++
@@ -1,8 +0,0 @@
-;; midnight-mode kills unused buffers.
-(require 'midnight)
-;; Kill special buffers untouched for X SECONDS.
-(setq clean-buffer-list-delay-special 300)
-;; Kill file buffers untouched for X DAYS.
-(setq clean-buffer-list-delay-general 4)
-;; Run clean-buffer-list every hour.
-(setq clean-buffer-list-timer (run-at-time t 3600 'clean-buffer-list))
