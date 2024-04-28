773b48dc4a7ed3538d400f7d9e1c7014ca35ca68
Jay McGavren <jay@mcgavren.com>
2024-04-28 14:23:01 -0400

    Extract ido-mode config to ido-mode.el

---
+++ ido-mode.el
@@ -0,0 +1,5 @@
+; ido normally lists options horizontally, which is highly unreadable.
+(require 'ido-vertical-mode)
+(ido-mode 1)
+(ido-vertical-mode 1)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
--- init.el
+++ init.el
@@ -18,12 +18,6 @@
 
 (setq ruby-deep-indent-paren nil)
 
-; ido normally lists options horizontally, which is highly unreadable.
-(require 'ido-vertical-mode)
-(ido-mode 1)
-(ido-vertical-mode 1)
-(setq ido-vertical-define-keys 'C-n-and-C-p-only)
-
 ; Test toggle setup.
 (global-set-key (kbd "C-c t") 'tgt-toggle)
 (setq tgt-open-in-new-window 'nil)
