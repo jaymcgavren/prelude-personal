fbaeb9e6b7af97a183838582c9f768d650b03e6e
Jay McGavren <jay@mcgavren.com>
2024-04-28 14:50:52 -0400

    Update to custom.el from another installation

--- custom.el
+++ custom.el
@@ -3,16 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(global-company-mode nil)
+ '(copilot-node-executable "/Users/jay/.nvm/versions/node/v16.20.1/bin/node")
  '(js-indent-level 2)
  '(package-selected-packages
-   '(org-jira chruby rubocop ruby-refactor textile-mode rspec-mode golint csharp-mode yasnippet scss-mode sass-mode yaml-mode zop-to-char zenburn-theme which-key volatile-highlights vkill undo-tree toggle-test smex smartrep smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile ov operate-on-number move-text markdown-mode magit json-mode js2-mode imenu-anywhere ido-vertical-mode ido-ubiquitous guru-mode grizzl god-mode go-mode gitignore-mode gitconfig-mode git-timemachine git-link gist geiser flycheck flx-ido expand-region exec-path-from-shell elisp-slime-nav editorconfig easy-kill dockerfile-mode discover-my-major diminish diff-hl csv-mode crux company coffee-mode browse-kill-ring beacon anzu ag ace-window))
- '(smartparens-global-mode nil))
+   '(textile-mode quelpa-use-package use-package quelpa rg go-mode scss-mode csv-mode ruby-refactor dockerfile-mode wgrep-ag pythonic anaconda-mode d-mode helm-projectile helm markdown-mode multiple-cursors rspec-mode toggle-test ido-vertical-mode git-link yaml-mode web-mode lsp-ui lsp-mode json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company counsel swiper ivy exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number nlinum move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring anzu ag ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight bold))))
- '(markdown-header-face-1 ((t (:inherit markdown-header-face :background "brightwhite" :foreground "blue"))))
- '(markdown-header-face-2 ((t (:inherit markdown-header-face :background "brightwhite" :foreground "blue")))))
+ )
