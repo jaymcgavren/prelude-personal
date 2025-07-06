9dcb2a4befb28ef613c6fd3ae66d3a5b810455f5
Jay McGavren <jay@mcgavren.com>
2025-07-06 00:29:39 -0700

    Stop requiring golint

--- golang.el
+++ golang.el
@@ -5,7 +5,6 @@
   (setq tab-width 4 indent-tabs-mode 1))
 (add-hook 'go-mode-hook 'my-go-mode-hook)
 (add-to-list 'load-path (concat (getenv "HOME")  "/go/src/github.com/golang/lint/misc/emacs"))
-(require 'golint)
 
 ;; Tabs are not a whitespace error in .go files; don't treat them as such.
 (setq whitespace-global-modes '(not go-mode))
