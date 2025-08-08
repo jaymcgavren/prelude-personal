d687877193e51ef66f2c909dd77326089b4494e4
Jay McGavren <jay@mcgavren.com>
2025-08-07 17:32:38 -0700

    Unset package-selected packages
    Don't know if this is advisable, but Emacs/Prelude did it on its own.

--- custom.el
+++ custom.el
@@ -3,17 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ace-window ag anaconda-mode anzu browse-kill-ring company consult copilot
-                counsel crux diff-hl diminish discover-my-major easy-kill
-                elisp-slime-nav epl exec-path-from-shell expand-region flx-ido
-                flycheck gist git-modes git-timemachine guru-mode hl-todo
-                ido-vertical-mode imenu-anywhere inf-ruby js2-mode json-mode
-                lsp-ui magit move-text nlinum operate-on-number orderless
-                projectile quelpa-use-package rainbow-delimiters rainbow-mode
-                smartparens smartrep super-save undo-tree vertico
-                volatile-highlights web-mode yaml-mode yari yasnippet
-                zenburn-theme zop-to-char)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
